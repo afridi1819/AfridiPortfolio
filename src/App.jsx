4b0afb36916f56197a8b70bc0d697d1c27c41878
afridi <shaikh19afridi@gmail.com>
2026-07-04 16:25:09 +0530

    Initial portfolio setup with navbar and hero section

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Navabar from "./components/Navbar/Navbar";
+
+
+function App(){
+
+  return(
+
+    <>
+    <Navabar />
+    </>
+
+  );
+
+}
+export default App;
+
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,10 @@
+import { nav } from "framer-motion/client";
+
+function Navabar() {
+    return (
+        <nav>
+            Navbar
+        </nav>
+    );
+}
+export default Navabar;
